226fae9815fffc0fc0b4fb54b70e29bbe8abe1d0
Artur Ribeiro <artur.felipe.ribeiro2010@gmail.com>
2024-11-17 21:58:11 -0300

    fix: remove logs

--- src/services/db.js
+++ src/services/db.js
@@ -1,4 +1,3 @@
-// let request;
 let db;
 let version = 1;
 
@@ -6,24 +5,19 @@ export const storeName = "cards";
 
 export const initDB = () => {
   return new Promise((resolve) => {
-    // open the connection
     const request = indexedDB.open("cardsDB");
 
     request.onupgradeneeded = () => {
       db = request.result;
 
-      // if the data object store doesn't exist, create it
       if (!db.objectStoreNames.contains(storeName)) {
-        console.log("Creating cards store");
         db.createObjectStore(storeName, { keyPath: "id", autoIncrement: true });
       }
-      // no need to resolve here
     };
 
     request.onsuccess = () => {
       db = request.result;
       version = db.version;
-      console.log("request.onsuccess - initDB", version);
       resolve({ success: true });
     };
 
@@ -38,7 +32,6 @@ export const getStoreData = (storeName) => {
     const request = indexedDB.open("cardsDB");
 
     request.onsuccess = () => {
-      console.log("request.onsuccess - getAllData");
       db = request.result;
       const tx = db.transaction(storeName, "readonly");
       const store = tx.objectStore(storeName);
@@ -55,7 +48,6 @@ export const addData = (storeName, data) => {
     const request = indexedDB.open("cardsDB", version);
 
     request.onsuccess = () => {
-      console.log("request.onsuccess - addData", data);
       db = request.result;
       const tx = db.transaction(storeName, "readwrite");
       const store = tx.objectStore(storeName);
@@ -77,30 +69,23 @@ export const addData = (storeName, data) => {
 export const updateData = (storeName, data) => {
   return new Promise((resolve) => {
     const request = indexedDB.open("cardsDB", version);
-    console.log({ data });
 
     request.onsuccess = () => {
-      console.log("request.onsuccess - addData", data);
       db = request.result;
       const tx = db.transaction(storeName, "readwrite");
       const store = tx.objectStore(storeName);
       const res = store.get(data.id);
       res.onsuccess = (event) => {
-        // Get the old value that we want to update
         let card = event.target.result;
 
-        // update the value(s) in the object that you want to change
         card = { ...card, ...data };
 
-        // Put this updated object back into the database.
         const requestUpdate = store.put(card);
         requestUpdate.onerror = (event) => {
           resolve({ success: false, error: "Unknown error" });
-          // Do something with the error
         };
         requestUpdate.onsuccess = (event) => {
           resolve({ success: true, data: card });
-          // Success - the data is updated!
         };
       };
       res.onerror = () => {
@@ -126,25 +111,20 @@ export const updateData = (storeName, data) => {
 
 export const deleteData = (storeName, key) => {
   return new Promise((resolve) => {
-    console.log({storeName, key})
-    // again open the connection
     const request = indexedDB.open("cardsDB", version);
 
     request.onsuccess = () => {
-      console.log("request.onsuccess - deleteData", key);
       db = request.result;
       const tx = db.transaction(storeName, "readwrite");
       const store = tx.objectStore(storeName);
       const res = store.delete(key);
-      
-      // add listeners that will resolve the Promise
+
       res.onsuccess = () => {
-        console.log('teste')
         resolve({ success: true });
       };
       res.onerror = () => {
-      const error = request.error?.message;
-      console.log({error})
+        const error = request.error?.message;
+        console.log({ error });
         resolve({ success: false, error: "Unknow error" });
       };
     };
